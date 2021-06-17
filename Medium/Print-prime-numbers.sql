f8fee956d74d7d95e3d72da6e5361efae33ed175
Eliannys <eliannyscer@gmail.com>
2021-06-17 11:49:31 +0200

    Added print prime numbers solution

---
+++ Medium/Print-prime-numbers.sql
@@ -0,0 +1,25 @@
+'''
+This challenge is special because printing prime 
+numbers is not what we typically do with SQL.
+'''
+
+
+
+DECLARE @table TABLE (PrimeNumber INT)
+DECLARE @final AS VARCHAR(1500)
+SET @final = ''
+DECLARE @counter INT
+SET @counter = 2
+WHILE @counter <= 1000
+BEGIN
+    IF NOT EXISTS (
+            SELECT PrimeNumber
+            FROM @table
+            WHERE @counter % PrimeNumber = 0)
+        BEGIN 
+            INSERT INTO @table SELECT @counter
+            SET @final = @final + CAST(@counter AS VARCHAR(20))+'&' 
+        END
+    SET @counter = @counter + 1
+END
+SELECT SUBSTRING(@final,0,LEN(@final))
